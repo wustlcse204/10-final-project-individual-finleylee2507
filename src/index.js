8542f85b3f485445e78a6006017ce6039d8d3e4f
finleylee2507 <46691077+finleylee2507@users.noreply.github.com>
2021-12-09 11:06:55 -0600

    Update

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { FavoritesContext, FavoritesContextProvider } from './store/favorites-context'
 
 
 
 ReactDOM.render(
   <FavoritesContextProvider>
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <App />
-    </BrowserRouter>
+    </BrowserRouter> */}
+
+    <HashRouter>
+      <App/>
+    </HashRouter>
 
   </FavoritesContextProvider>
   ,
