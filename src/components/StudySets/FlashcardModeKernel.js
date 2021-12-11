24b9327424fa4d308be94cd449a813f8bd6671b9
finleylee2507 <46691077+finleylee2507@users.noreply.github.com>
2021-12-11 17:18:10 -0600

    New update!

--- src/components/StudySets/FlashcardModeKernel.js
+++ src/components/StudySets/FlashcardModeKernel.js
@@ -1,51 +1,63 @@
 import React, { useContext, useEffect } from 'react'
-import classes from './FlashcardModeKernel.module.css'
+import './FlashcardModeKernel.css';
 import usePersistedState from '../../storage/customHooks';
 import { CurrentSetContext } from '../../storage/current-set-context';
 import { isLoadingContext } from '../../pages/AllStudySets';
 
 const FlashcardModeKernel = () => {
 
-    const currentSetCtx=useContext(CurrentSetContext);
-  
-    // const [currentIndex, setCurrentIndex] = usePersistedState("currIndex", 0);
-    // const [currentItem, setCurrentItem] = usePersistedState("currItem", "");
-    // const [currentData, setCurrentData] = usePersistedState("currData", data);
-    // const [currentMode, setCurrentMode] = usePersistedState("currMode", "term"); //0 ->term first, 1 ->definition first
-    const currentIndex=currentSetCtx.currentIndex;
-    const currentData=currentSetCtx.currentList;
-    const currentMode=currentSetCtx.currentMode;
- 
+    const currentSetCtx = useContext(CurrentSetContext);
+
+
+    const currentIndex = currentSetCtx.currentIndex;
+    const currentData = currentSetCtx.currentList;
+    const currentMode = currentSetCtx.currentMode;
+
 
     // console.log(currentData);
 
     //  console.log(currentIndex);
 
-    
-     const currentItem=currentSetCtx.currentList[currentIndex];
+
+    const currentItem = currentSetCtx.currentList[currentIndex];
 
     // console.log(currentItem);
- 
+
     // console.log(currentMode);
-  
 
 
+    const flipCardAnimation=()=>{
+        document.getElementsByClassName('card')[0].classList.toggle('is-flipped');
+    }
 
     return (
-        <div className={classes.card}>
 
+        //citation:https://3dtransforms.desandro.com/card-flip
+        <div className='cardScene'>
 
-            {/* <a className={classes.displayArea}>{currentItem[currentMode] ? <p>{currentItem[currentMode]}</p> : <p>Loading...</p>}</a> */}
+            <div className='card' onClick={()=>{flipCardAnimation();}}>
+                <div className='cardFace cardFaceFront'>
+                    <a className='displayArea'><p>{currentItem['term']}</p></a>
+                </div>
+                <div className='cardFace cardFaceBack'>
 
-            <a className={classes.displayArea}><p>{currentItem[currentMode]}</p></a>
-            <div className={classes.actionCenter}>
+                    <a className='displayArea'><p>{currentItem['definition']}</p></a>
+                </div>
 
-                <a className={classes.previousBtn} title="previous" onClick={()=>{currentSetCtx.previousItemHandler();}}> &#10094; </a>
-                <button className={classes.flipBtn} onClick={()=>{currentSetCtx.flipHandler();}} > Flip Card</button>
-                <a className={classes.nextBtn} title="next" onClick={()=>{currentSetCtx.nextItemHandler();}}>&#10095;</a>
 
 
             </div>
+
+
+            <div className='actionCenter'>
+
+                <a className='previousBtn' title="previous" onClick={() => { currentSetCtx.previousItemHandler(); }}> &#10094; </a>
+                <button className='flipBtn' onClick={() => {flipCardAnimation(); }} > Flip Card</button>
+                <a className='nextBtn' title="next" onClick={() => { currentSetCtx.nextItemHandler(); }}>&#10095;</a>
+
+
+            </div>
+
 
         </div>
     )
